573b71f453f74d2457b309dc38c74e2e498221d3
harishtpj <harishtpj@hotmail.com>
2023-08-28 19:55:25 +0530

    Initialized project

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#ifndef HCLANG_COMMON_H
+#define HCLANG_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
---
+++ src/value.h
@@ -0,0 +1,19 @@
+#ifndef HCLAN_VALUE_H
+#define HCLAN_VALUE_H
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+  int capacity;
+  int count;
+  Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
+#endif
